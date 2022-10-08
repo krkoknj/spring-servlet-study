6f2e817085e14bc77b0e1033fba2bff0342c67f0
¤knj <krkoknj@gmail.com>
2022-10-08 16:30:06 +0900

    first commit

---
+++ src/main/java/com/study/servlet/basic/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.study.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+
+        res.setContentType("text/plain");
+        res.setCharacterEncoding("utf-8");
+        res.getWriter().write("hello" + username);
+    }
+}
